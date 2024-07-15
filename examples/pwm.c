a6793e13c70d20b9cabe3369b5f3e2f094cdecc6
August <2819763+Dangku@users.noreply.github.com>
2024-07-15 19:52:11 +0800

    update pwm example for bananapi

--- examples/pwm.c
+++ examples/pwm.c
@@ -32,24 +32,24 @@ int main (void)
 {
   int bright ;
 
-  printf ("Raspberry Pi wiringPi PWM test program\n") ;
+  printf ("Bananapi wiringPi PWM test program\n") ;
 
   if (wiringPiSetup () == -1)
     exit (1) ;
 
-  pinMode (1, PWM_OUTPUT) ;
+  pinMode (7, PWM_OUTPUT) ;
 
   for (;;)
   {
     for (bright = 0 ; bright < 1024 ; ++bright)
     {
-      pwmWrite (1, bright) ;
+      pwmWrite (7, bright) ;
       delay (1) ;
     }
 
     for (bright = 1023 ; bright >= 0 ; --bright)
     {
-      pwmWrite (1, bright) ;
+      pwmWrite (7, bright) ;
       delay (1) ;
     }
   }
